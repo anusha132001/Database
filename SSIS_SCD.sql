b567ca7f18972d7aa1bb4ee8dce817c3aa5849db
Anusha Shetty <anushashetty1303@gmail.com>
2022-09-28 17:58:07 +0530

    Commit
    Commit Changes

--- SSIS_SCD.sql
+++ SSIS_SCD.sql
@@ -44,8 +44,23 @@ SELECT * from EMPLOYEE_T
 
 UPDATE EMPLOYEE  SET PLACE='Punjab' WHERE ID=102;
 
-SELECT distinct city from HEALTH_RECORD hr 
+SELECT distinct city from Sales_Data sd 
 
-select * from HEALTH_RECORD hr where Segment ='Consumer' and City in('Orange','Baltimore')
+select * from BANK_DATA hr 
 
-SELECT DISTINCT CITY FROM HEALTH_RECORD;
+---TOTAL NUMBER OF BANK DEPLOYED IN A YEAR----
+select year,count(bank_name) from BANK_DATA bd 
+group by year
+
+---THE TOTAL NUMBER OF ATM DEPLOYED IN ONSITE FOR PARTICULAR BANK IN A YEAR--- 
+select year,bank_name ,sum(no_atms_on_site)as ATM from BANK_DATA bd 
+group by year,bank_name 
+
+---BANKS IN A YEAR--------
+
+select year ,count(bank_name )as Num_Bank from BANK_DATA bd 
+group by year
+
+SELECT DISTINCT bank_name from BANK_DATA bd 
+
+select * from BANK_DATA bd where bank_name ='AMERICAN EXPRESS' and year='2015'
